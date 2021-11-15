373761c4d58380009b7576a0409940a4370b2602
Wolfgang Werner <mail@wolfgang-werner.net>
2021-11-15 12:00:08 +0100

    Add player PoC

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { Pi } from "./components/Pi";
+import Player from "./components/Player";
 
-ReactDOM.render(<Pi />, document.getElementById("root"));
+ReactDOM.render(<div className="pi"><Pi /><Player /></div>, document.getElementById("root"));
